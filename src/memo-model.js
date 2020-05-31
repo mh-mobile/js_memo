8c26d6e05c46832ff19b00e4ab4a9498efe8af65
mbyk <marubayashi@gmail.com>
2020-05-31 18:13:03 +0900

    MemoModelクラスを追加

---
+++ src/memo-model.js
@@ -0,0 +1,9 @@
+class MemoModel {
+  constructor (title, createAt, updatedAt) {
+    this.title = title
+    this.createAt = createAt
+    this.updatedAt = updatedAt
+  }
+}
+
+module.exports = MemoModel
