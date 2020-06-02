8def950b946109f472133bbcebfe0c3646538365
mbyk <marubayashi@gmail.com>
2020-06-02 14:04:36 +0900

    メモの参照処理のリファクタ

--- src/store/memo-file-store.js
+++ src/store/memo-file-store.js
@@ -31,25 +31,8 @@ class MemoFileStore extends MemoStore {
   }
 
   read (id) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(this.filePath, 'utf-8', (error, data) => {
-        if (error) {
-          if (error.code === 'ENOENT') {
-            resolve([])
-            return
-          }
-
-          reject(error)
-          return
-        }
-
-        const jsonArray = JSON.parse(data)
-        const memos = jsonArray.map((memoJson) => {
-          return new MemoModel(memoJson.content, memoJson.id)
-        })
-        const foundMemo = memos.find((memo) => memo.id === id)
-        resolve(foundMemo)
-      })
+    return this.list().then((memos) => {
+      return memos.find((memo) => memo.id === id)
     })
   }
 
