8767365473615e7538ab2719ae68380d439db9e7
mbyk <marubayashi@gmail.com>
2020-06-05 13:07:16 +0900

    一覧表示をprototypeで変換

--- src/store/memo-model.js
+++ src/store/memo-model.js
@@ -1,16 +1,19 @@
-class MemoModel {
-  constructor (id, content) {
-    this.id = id
-    this.content = content
-  }
 
-  get name () {
-    return this.content.split('\n')[0]
-  }
-
-  get value () {
-    return this.id
-  }
+function MemoModel (id, content) {
+  this.id = id
+  this.content = content
+  this.name = this._name()
+  this.value = this._value()
 }
+
+MemoModel.prototype._name = function () {
+  return this.content.split('\n')[0]
+}
+
+MemoModel.prototype._value = function () {
+  return this.id
+}
+
+MemoModel.prototype.constructor = MemoModel
 
 module.exports = MemoModel
