837ee96f415c6ee8068517a923f8e9e809e0211f
mbyk <marubayashi@gmail.com>
2020-06-05 13:50:18 +0900

    MemoSqliteStoreクラスをprototypeで変換

--- src/store/memo-sqlite-store.js
+++ src/store/memo-sqlite-store.js
@@ -1,109 +1,111 @@
 const sqlite3 = require('sqlite3')
 const MemoModel = require('./memo-model.js')
 const uuid = require('node-uuid')
+const MemoStore = require('./memo-store.js')
 
-class MemoSqliteStore {
-  constructor (filePath) {
-    this.initTable(filePath)
-  }
+function MemoSqliteStore (filePath) {
+  MemoStore.call(this, filePath)
+  this.initTable(filePath)
+}
 
-  async initTable (filePath) {
-    this.database = new sqlite3.Database(filePath)
-    await this.createTableIfNeeds()
-  }
+MemoSqliteStore.prototype = Object.create(MemoStore.prototype)
+MemoSqliteStore.prototype.constructor = MemoSqliteStore
+MemoSqliteStore.prototype.initTable = async function (filePath) {
+  this.database = new sqlite3.Database(filePath)
+  await this.createTableIfNeeds()
+}
 
-  getDatabase () {
-    return this.database
-  }
+MemoSqliteStore.prototype.getDatabase = function () {
+  return this.database
+}
 
-  getTableName () {
-    return 'memos'
-  }
+MemoSqliteStore.prototype.getTableName = function () {
+  return 'memos'
+}
 
-  createTableIfNeeds () {
-    return new Promise((resolve) => {
-      this.getDatabase().serialize(() => {
-        this.getDatabase().run(`create table if not exists ${this.getTableName()} (
-          id text primary key,
-          content text
-        )`)
-      })
-      return resolve()
+MemoSqliteStore.prototype.createTableIfNeeds = function () {
+  return new Promise((resolve) => {
+    this.getDatabase().serialize(() => {
+      this.getDatabase().run(`create table if not exists ${this.getTableName()} (
+        id text primary key,
+        content text
+      )`)
     })
-  }
+    return resolve()
+  })
+}
 
-  list () {
-    return new Promise((resolve) => {
-      this.getDatabase().serialize(() => {
-        this.getDatabase().all(`select * from ${this.getTableName()}`, (error, rows) => {
-          if (error) throw error
-          const memos = rows.map((row) => {
-            return new MemoModel(row.id, row.content)
-          })
-          resolve(memos)
+MemoSqliteStore.prototype.list = function () {
+  return new Promise((resolve) => {
+    this.getDatabase().serialize(() => {
+      this.getDatabase().all(`select * from ${this.getTableName()}`, (error, rows) => {
+        if (error) throw error
+        const memos = rows.map((row) => {
+          return new MemoModel(row.id, row.content)
         })
+        resolve(memos)
       })
     })
-  }
+  })
+}
 
-  read (id) {
-    return new Promise((resolve, reject) => {
-      this.getDatabase().serialize(() => {
-        this.getDatabase().get(`select id, content from ${this.getTableName()} where id = "${id}"`, (error, row) => {
-          if (error) return reject(error)
-          const memo = new MemoModel(uuid.v4(), row.content)
-          resolve(memo)
+MemoSqliteStore.prototype.read = function (id) {
+  return new Promise((resolve, reject) => {
+    this.getDatabase().serialize(() => {
+      this.getDatabase().get(`select id, content from ${this.getTableName()} where id = "${id}"`, (error, row) => {
+        if (error) return reject(error)
+        const memo = new MemoModel(uuid.v4(), row.content)
+        resolve(memo)
+      })
+    })
+  })
+}
+
+MemoSqliteStore.prototype.update = function (editedMemo) {
+  return new Promise((resolve, reject) => {
+    this.getDatabase().serialize(() => {
+      this.getDatabase().run('begin transaction')
+      try {
+        this.getDatabase().run(`update ${this.getTableName()} set content = "${editedMemo.content}" where id = "${editedMemo.id}"`)
+        this.getDatabase().run('commit')
+        return resolve()
+      } catch (error) {
+        this.getDatabase().run('rollback')
+        reject(error)
+      }
+    })
+  })
+}
+
+MemoSqliteStore.prototype.delete = function (id) {
+  return new Promise((resolve, reject) => {
+    this.getDatabase().serialize(() => {
+      try {
+        this.getDatabase().run(`delete from ${this.getTableName()} where id = "${id}"`)
+        return resolve()
+      } catch (error) {
+        reject(error)
+      }
+    })
+  })
+}
+
+MemoSqliteStore.prototype.create = function (newMemos) {
+  return new Promise((resolve, reject) => {
+    this.getDatabase().serialize(() => {
+      this.getDatabase().run('begin transaction')
+      try {
+        newMemos.forEach((memo) => {
+          this.getDatabase().run(`insert into ${this.getTableName()} (id, content) values ($id, $content)`, [memo.id, memo.content])
         })
-      })
+        this.getDatabase().run('commit')
+        return resolve()
+      } catch (error) {
+        this.getDatabase().run('rollback')
+        reject(error)
+      }
     })
-  }
-
-  update (editedMemo) {
-    return new Promise((resolve, reject) => {
-      this.getDatabase().serialize(() => {
-        this.getDatabase().run('begin transaction')
-        try {
-          this.getDatabase().run(`update ${this.getTableName()} set content = "${editedMemo.content}" where id = "${editedMemo.id}"`)
-          this.getDatabase().run('commit')
-          return resolve()
-        } catch (error) {
-          this.getDatabase().run('rollback')
-          reject(error)
-        }
-      })
-    })
-  }
-
-  delete (id) {
-    return new Promise((resolve, reject) => {
-      this.getDatabase().serialize(() => {
-        try {
-          this.getDatabase().run(`delete from ${this.getTableName()} where id = "${id}"`)
-          return resolve()
-        } catch (error) {
-          reject(error)
-        }
-      })
-    })
-  }
-
-  create (newMemos) {
-    return new Promise((resolve, reject) => {
-      this.getDatabase().serialize(() => {
-        this.getDatabase().run('begin transaction')
-        try {
-          newMemos.forEach((memo) => {
-            this.getDatabase().run(`insert into ${this.getTableName()} (id, content) values ($id, $content)`, [memo.id, memo.content])
-          })
-          this.getDatabase().run('commit')
-          return resolve()
-        } catch (error) {
-          this.getDatabase().run('rollback')
-          reject(error)
-        }
-      })
-    })
-  }
+  })
 }
 
 module.exports = MemoSqliteStore
