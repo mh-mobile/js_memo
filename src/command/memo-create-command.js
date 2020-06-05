91fa52f1241b19a5216ac3309bb80453153c4791
mbyk <marubayashi@gmail.com>
2020-06-05 13:16:01 +0900

    MemoCreateCommandクラスをprototypeで書き換え

--- src/command/memo-create-command.js
+++ src/command/memo-create-command.js
@@ -1,27 +1,29 @@
 const { MemoCommand } = require('./memo-command.js')
 const FileUtil = require('../util/file-util.js')
 
-class MemoCreateCommand extends MemoCommand {
-  constructor (dao, filePaths) {
-    super(dao)
-    this.filePaths = filePaths
-  }
+function MemoCreateCommand (dao, filePaths) {
+  MemoCommand.call(this, dao, filePaths)
+  this.filePaths = filePaths
+}
 
-  async execute () {
-    const memos = await this.createMemos()
-    if (memos.length === 0) return
-    await this.dao.create(memos)
-  }
+MemoCreateCommand.prototype = Object.create(MemoCommand.prototype)
 
-  async createMemos () {
-    if (FileUtil.filePathExists(this.filePaths)) {
-      // 引数のファイルからメモを追加
-      return FileUtil.convertFilePathsToMemos(this.filePaths)
-    } else {
-      // 標準入力からメモを追加
-      return FileUtil.convertStdinToMemos()
-    }
+MemoCreateCommand.prototype.execute = async function () {
+  const memos = await this.createMemos()
+  if (memos.length === 0) return
+  await this.dao.create(memos)
+}
+
+MemoCreateCommand.prototype.createMemos = async function () {
+  if (FileUtil.filePathExists(this.filePaths)) {
+    // 引数のファイルからメモを追加
+    return FileUtil.convertFilePathsToMemos(this.filePaths)
+  } else {
+    // 標準入力からメモを追加
+    return FileUtil.convertStdinToMemos()
   }
 }
+
+MemoCreateCommand.prototype.constructor = MemoCreateCommand
 
 module.exports = MemoCreateCommand
