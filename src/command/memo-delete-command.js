4b7d6565434d338557af81474cb11799695a2754
mbyk <marubayashi@gmail.com>
2020-06-05 13:22:50 +0900

    MemoDelteCommandクラスをprototypeで変換

--- src/command/memo-delete-command.js
+++ src/command/memo-delete-command.js
@@ -1,24 +1,29 @@
 const { MemoCommand, inquirer } = require('./memo-command.js')
 
-class MemoDeleteCommand extends MemoCommand {
-  async execute () {
-    const memos = await this.dao.list()
-    if (memos.length === 0) return
-
-    inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'memo',
-          message: 'Choose a note you want to delete:',
-          choices: memos
-        }
-      ])
-      .then(async (answers) => {
-        const selectedMemo = memos.find((memo) => memo.value === answers.memo)
-        await this.dao.delete(selectedMemo.id)
-      })
-  }
+function MemoDeleteCommand (dao) {
+  MemoCommand.call(this, dao)
 }
+
+MemoDeleteCommand.prototype = Object.create(MemoCommand.prototype)
+MemoDeleteCommand.prototype.execute = async function () {
+  const memos = await this.dao.list()
+  if (memos.length === 0) return
+
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'memo',
+        message: 'Choose a note you want to delete:',
+        choices: memos
+      }
+    ])
+    .then(async (answers) => {
+      const selectedMemo = memos.find((memo) => memo.value === answers.memo)
+      await this.dao.delete(selectedMemo.id)
+    })
+}
+
+MemoDeleteCommand.prototype.constructor = MemoDeleteCommand
 
 module.exports = MemoDeleteCommand
