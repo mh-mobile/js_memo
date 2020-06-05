0c801f405e0785407367defc8262d8c247bd3abb
mbyk <marubayashi@gmail.com>
2020-06-05 13:33:06 +0900

    MemoReadCommandクラスをprototypeで変換

--- src/command/memo-read-command.js
+++ src/command/memo-read-command.js
@@ -1,25 +1,29 @@
 const { MemoCommand, inquirer } = require('./memo-command.js')
 
-class MemoReadCommand extends MemoCommand {
-  async execute () {
-    const memos = await this.dao.list()
-    if (memos.length === 0) return
-
-    inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'memo',
-          message: 'Choose a note you want to see:',
-          choices: memos
-        }
-      ])
-      .then(async (answers) => {
-        const selectedMemo = memos.find((memo) => memo.value === answers.memo)
-        const memo = await this.dao.read(selectedMemo.id)
-        console.log(memo.content)
-      })
-  }
+function MemoReadCommand (dao) {
+  MemoCommand.call(this, dao)
 }
+
+MemoReadCommand.prototype = Object.create(MemoCommand.prototype)
+MemoReadCommand.prototype.execute = async function () {
+  const memos = await this.dao.list()
+  if (memos.length === 0) return
+
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'memo',
+        message: 'Choose a note you want to see:',
+        choices: memos
+      }
+    ])
+    .then(async (answers) => {
+      const selectedMemo = memos.find((memo) => memo.value === answers.memo)
+      const memo = await this.dao.read(selectedMemo.id)
+      console.log(memo.content)
+    })
+}
+MemoReadCommand.prototype.constructor = MemoReadCommand
 
 module.exports = MemoReadCommand
